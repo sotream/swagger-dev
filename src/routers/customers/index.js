1d409b0432fb10174ffc4f79c3cab48bb21d0792
Andrii Prisniak <andrey.prisniak@gmail.com>
2023-10-22 18:23:29 +0300

    feat(source): add endpoint for getting all users

--- src/routers/customers/index.js
+++ src/routers/customers/index.js
@@ -41,6 +41,53 @@ customersRouter.post('/', async (req, res) => {
   }
 });
 
+customersRouter.get('/', async (req, res) => {
+  const defaultPageSize = 25;
+  const defaultPageNumber = 1;
+
+  log.info(
+    `Get all customers request query params ${JSON.stringify(
+      req.query,
+    )} default page size ${defaultPageSize} default page number ${defaultPageNumber}`,
+  );
+
+  try {
+    const { size = defaultPageSize, page = defaultPageNumber } = req.query;
+    const rawCustomers = await db.Customer.findAll({
+      where: {},
+      limit: Number(size),
+      offset: (Number(page) - 1) * Number(size),
+      order: [['id', 'DESC']],
+    });
+
+    const customers = rawCustomers.map((customer) => {
+      return {
+        id: customer.id,
+        name: customer.name,
+        email: customer.email,
+        created: customer.createdAt,
+        updated: customer.updatedAt,
+      };
+    });
+
+    log.debug('All customers', JSON.stringify(customers));
+
+    res.status(200).json({
+      status: 'ok',
+      data: customers,
+    });
+  } catch (error) {
+    log.error('Get all customers error', error);
+
+    res.status(error.code || 400).json({
+      status: 'error',
+      data: {
+        message: error.message || 'Internal error',
+      },
+    });
+  }
+});
+
 module.exports = {
   customersRouter,
 };
